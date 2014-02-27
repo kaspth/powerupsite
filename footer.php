edc26137855d32ce9b132109c6dcb606d42a42c7
Timm <kaspth@gmail.com>
2014-02-27 10:43:56 +0100

    Added facebook page link to footer.

--- footer.php
+++ footer.php
@@ -2,6 +2,6 @@
   <ul>
     <li><a href="contact.html">Kontakt os</a></li>
     <li><a href="about.html">Om os</a></li>
-    <li><a href="facebook_page.html">Like os...</a></li>
+    <li><a href="https://www.facebook.com/pages/Power-Up/460877770705249">Like os...</a></li>
   </ul>
 </footer>
